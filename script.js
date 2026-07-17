668e773be09170020902d66315407a79dee4c4e3
peter-conder <pconder2005@gmail.com>
2026-07-17 10:06:57 -0600

    add basic files, add math functions

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(a,b) {
+    return a + b;
+}
+
+function subtract(a,b) {
+    return a - b;
+}
+
+function multiply(a,b) {
+    return a * b;
+}
+
+function divide(a,b) {
+    return a / b;
+}
+
+console.log(add(1,2));
+console.log(subtract(1000, 788));
+console.log(multiply(2,3));
+console.log(multiply(9,9));
