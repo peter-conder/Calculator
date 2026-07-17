a26b652798c7560bec7e5399197ad3c42d245fe0
peter-conder <pconder2005@gmail.com>
2026-07-17 16:51:26 -0600

    add clear button, add division rounding

--- script.js
+++ script.js
@@ -22,10 +22,7 @@ let result;
 
 
 let display = document.querySelector(".display");
-//display.textContent = number;
-
 const buttons = document.querySelectorAll(".notEnter");
-
 let operation = [];
 
 
@@ -36,35 +33,41 @@ buttons.forEach((button) => {
         number = button.dataset.value;
         //display.textContent = number;
         operation.push(number);
-        let operationString = operation.toString();
     })
 });
 
 function operate(number, number2, operator) {
-    let result;
     if (operator === "+") {
         let int1 = Number(number);
         let int2 = Number(number2);
-    result = (add(int1, int2));
-    return result;
+    return (add(int1, int2)); 
     } else if (operator === "-") {
-        result = (subtract(number, number2));
-        return result;
+        return (subtract(number, number2));
     } else if (operator === "*") {
-        result = (multiply(number, number2));
-        return result;
+        return (multiply(number, number2));
     } else if (operator === "/") {
-        result = (divide(number, number2));
-        return result;
+        let divResult = (divide(number, number2));
+        let roundResult = Math.round(divResult * 100) / 100;  
+        return roundResult;
     }
 }
 
 const enter = document.querySelector("#enter");
-
 enter.addEventListener("click", () => {
-    result2 = operate(operation.at(0), operation.at(2), operation.at(1));
-    console.log(result2);
+    result = operate(operation.at(0), operation.at(2), operation.at(1));
+    console.log(result);
     operation.length = 0;
-    operation.push(result2);
+    operation.push(result);
+    display.textContent = result;
 
-})
+});
+
+
+const clear = document.querySelector(".clear");
+clear.addEventListener("click", () => {
+    operation.length = 0;
+    if (result) {
+        result = 0;
+        display.textContent = "0";
+    };
+});
